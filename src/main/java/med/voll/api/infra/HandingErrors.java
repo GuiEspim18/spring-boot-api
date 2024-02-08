a11abb259774439228c8af70052a8555a00feae3
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-02-08 20:13:25 -0300

    handing errors

--- src/main/java/med/voll/api/infra/HandingErrors.java
+++ src/main/java/med/voll/api/infra/HandingErrors.java
@@ -3,8 +3,12 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class HandingErrors {
@@ -12,6 +16,18 @@ public class HandingErrors {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handError404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handError400(MethodArgumentNotValidException e) {
+        List<FieldError> errors = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DataErrorsValidation::new).toList());
+    }
+
+    private record DataErrorsValidation(String field, String message) {
+        public DataErrorsValidation (FieldError err) {
+            this(err.getField(), err.getDefaultMessage());
+        }
     }
 
 }
