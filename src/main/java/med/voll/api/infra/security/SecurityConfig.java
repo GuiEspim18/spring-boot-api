29dc2e1caefaf0765b257aed46f1cd3112d64ebb
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-02-08 22:02:15 -0300

    add users migrantion and entity with the auth service and security configurations

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // to disable the default login page from spring security
+
+    @Bean // expose the return of the method and give up an object to spring
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
