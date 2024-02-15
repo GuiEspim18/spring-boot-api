938e5628994f4c502e021e66ec7f2c9cbb79b059
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-02-15 17:11:35 -0300

    create appointment entity

---
+++ src/main/java/med/voll/api/domain/appointment/entity/Appointment.java
@@ -0,0 +1,34 @@
+package med.voll.api.domain.appointment.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.doctor.entity.Doctor;
+import med.voll.api.domain.patients.entity.Patient;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Appointments")
+@Table(name = "appointments")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    private LocalDateTime date;
+}
