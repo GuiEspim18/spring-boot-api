e2cfc65c6f074de9c5cb60c4a440637e0141d94d
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-02-15 17:05:52 -0300

    add appointment datails data

---
+++ src/main/java/med/voll/api/domain/appointment/AppointmentDetailsData.java
@@ -0,0 +1,11 @@
+package med.voll.api.domain.appointment;
+
+import java.time.LocalDateTime;
+
+public record AppointmentDetailsData(
+        Long id,
+        Long doctorId,
+        Long patientId,
+        LocalDateTime date
+) {
+}
